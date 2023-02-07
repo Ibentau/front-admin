6320bf2609bfe8675fe415d1d3eedec0dd965c06
Jerome <hikuluno@gmail.com>
2023-02-07 10:46:29 +0100

    creationBot page finish

--- src/routes/creationChatbot/+page.svelte
+++ src/routes/creationChatbot/+page.svelte
@@ -1,38 +1,36 @@
 <script>
+  import Step from "./step.svelte";
   let name = "";
-  let event = "";
+  let color = "#FFFFFF";
 </script>
 
-<h1>Chatbot creation</h1>
+<div class="flex flex-wrap h-screen justify-center content-center ">
+  <div class="flex flex-col ">
+    <div class="text-5xl font-bold">Chatbot creation</div>
 
-<div class="form-control w-full max-w-xs">
-  <label for="input" class="label">
-    <span class="label-text">Chatbot name</span>
-  </label>
-  <input
-    placeholder="Jean"
-    class="input input-bordered w-full max-w-xs"
-    bind:value={name}
-  />
+    <div class="form-control w-full max-w-xs mt-4">
+      <label for="input" class="label">
+        <span class="label-text">Chatbot name</span>
+      </label>
+      <input
+        placeholder="Jean"
+        class="input input-bordered w-full max-w-xs"
+        bind:value={name}
+      />
+    </div>
+    <div class="mt-4">
+      <label>
+        Theme color :
+        <input type="color" bind:value={color} class="hidden rounded-full" />
+        <span
+          class="rounded-full w-8 h-8 border-gray-500 border inline-block"
+          style="background-color: {color};"
+        />
+      </label>
+    </div>
+    <button class="btn mt-8">Confirm</button>
+  </div>
+  <div class="m-12">
+    <Step />
+  </div>
 </div>
-
-<div class="form-control w-full max-w-xs">
-  <label for="input" class="label">
-    <span class="label-text">Type of event </span>
-  </label>
-  <input
-    placeholder="Conference	"
-    class="input input-bordered w-full max-w-xs"
-    bind:value={event}
-  />
-</div>
-<br />
-<button class="btn">Confirm</button>
-
-<style>
-  h1 {
-    text-align: center;
-    font-size: 48px;
-    font-weight: bold;
-  }
-</style>
---
+++ src/routes/creationChatbot/step.svelte
@@ -0,0 +1,5 @@
+<ul class="steps steps-vertical">
+  <li class="step step-primary">Create Chatbot</li>
+  <li class="step">Customs data</li>
+  <li class="step">Finish</li>
+</ul>
