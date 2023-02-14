d1a250d790f77792be5d21c8b6294655afb97015
Jerome <hikuluno@gmail.com>
2023-02-14 10:26:27 +0100

    tutorial page

--- src/routes/creationChatbot/finish/+page.svelte
+++ src/routes/creationChatbot/finish/+page.svelte
@@ -3,14 +3,21 @@
   import DownloadJSON from "$lib/DownloadJSON.svelte";
 </script>
 
-<div class="flex flex-wrap h-screen justify-center content-center ">
+<div class="flex flex-wrap h-screen justify-center content-center">
   <div class="flex flex-col items-center justify-center">
-    <h1 class="text-4xl font-bold m-4 ">Finished !</h1>
-    <span class="m-4"
-      >That's it, you can download your JSON config file to deploy your Chatbot</span
-    >
+    <h1 class="text-4xl font-bold m-4">Finished!</h1>
+    <span class="m-4">
+      That's it, you can download your JSON config file to deploy your Chatbot
+    </span>
     <DownloadJSON />
+    <p class="mt-16 text-xl">
+      If you don't know how to use this .json file, follow this <a
+        class="underline "
+        href="/tutorial">tutorial</a
+      >
+    </p>
   </div>
+
   <div class="m-12">
     <Step currentstep={3} />
   </div>
